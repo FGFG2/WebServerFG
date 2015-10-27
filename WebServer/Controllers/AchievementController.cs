dc6da351853a44ce8c2caf701a14e149f731c06a
Jonas Hashagen <jonashashagen@gmail.com>
2015-10-27 16:31:31 +0100

    Added AchievementController and model. (Mocked data) refs #160

---
+++ WebServer/Controllers/AchievementController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServer.Models;
+
+namespace WebServer.Controllers
+{
+    /// <summary>
+    /// Provides the RESTful API regarding the achievements.
+    /// </summary>
+    public class AchievementController : ApiController
+    {
+        #region Mocked data
+        private readonly IEnumerable<Achievement> _mockedAchievements = new[]
+        {
+            new Achievement {Id = 0, Name = "Amateur", Description = "Finished 10 flights.", Progress = 70},
+            new Achievement {Id = 1, Name = "Lucky pilot", Description = "", Progress = 100},
+            new Achievement {Id = 2, Name = "Stunt pilot", Description = "", Progress = 0},
+            new Achievement
+            {
+                Id = 3,
+                Name = "Racing pilot",
+                Description = "Maximum power for 10 seconds.",
+                Progress = 20
+            },
+            new Achievement
+            {
+                Id = 4,
+                Name = "Kamikaze",
+                Description = "Managed to be in a power dive for 5 seconds.",
+                ImageUrl =
+                    @"https://de.wikipedia.org/wiki/Sturzflug#/media/File:F14bomb.jpg",
+                Progress = 100
+            }
+        };
+        #endregion
+        
+        // GET: api/AllAchievements
+        [Route("api/AllAchievements")]
+        public IEnumerable<Achievement> GetAllAchievements()
+        {
+            return _mockedAchievements.AsQueryable();
+        }
+
+        // GET: api/ObtainedAchievements
+        [Route("api/ObtainedAchievements")]
+        public IEnumerable<Achievement> GetObtainedAchievements()
+        {
+            var obtained =
+                from achievement in _mockedAchievements
+                where achievement.Progress == 100
+                select achievement;
+
+            return obtained.AsQueryable();
+        }
+    }
+}
---
+++ WebServer/Models/Achievement.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebServer.Models
+{
+    /// <summary>
+    /// Model class for a single achievement. Contains detailed information and the progress.
+    /// </summary>
+    public class Achievement
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the achievement.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Detailed description containing the goals needed to obtain the achievement.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// URL to the image used for the visual representation.
+        /// </summary>
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// Progress of the achievement in percent. A value of 100 means it has been finished.
+        /// </summary>
+        public byte Progress { get; set; }
+    }
+}
