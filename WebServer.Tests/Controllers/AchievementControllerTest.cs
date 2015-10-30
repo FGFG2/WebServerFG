2a4b6d89b06a1384e47c0be3221667dab54bf71e
Portikus <ahuisf@gmail.com>
2015-10-30 16:48:35 +0100

    Removed unused Controller. Added test for the PlaneDataController and for the AchievementController

---
+++ WebServer.Tests/Controllers/AchievementControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Universial.Test;
+using WebServer.Controllers;
+
+namespace WebServer.Tests.Controllers
+{
+    public class AchievementControllerTest : TestBase<AchievementController>
+    {
+        [SetUp]
+        protected override void SetUp()
+        {
+            base.SetUp();
+            SystemUnderTest = new AchievementController();
+        }
+
+        [Test]
+        public void Test_GetAllAchievements()
+        {
+            //Arrange 
+            
+            //Act
+            var result = SystemUnderTest.GetAllAchievements();
+
+            //Assert
+            Assert.That(()=>result,Is.Not.Null);
+        }
+
+        [Test]
+        public void Test_GetObtainedAchievements()
+        {
+            //Arrange 
+
+            //Act
+            var result = SystemUnderTest.GetObtainedAchievements();
+
+            //Assert
+            Assert.That(() => result, Is.Not.Null);
+        }
+    }
+}
---
+++ WebServer.Tests/Controllers/PlaneDataControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Universial.Test;
+using WebServer.Controllers;
+
+namespace WebServer.Tests.Controllers
+{
+    public class PlaneDataControllerTest :TestBase<PlaneDataController>
+    {
+        [SetUp]
+        protected override void SetUp()
+        {
+            base.SetUp();
+            SystemUnderTest = new PlaneDataController();
+        }
+
+        [Test]
+        public void Test_SetMotor()
+        {
+            //Arrange 
+            
+            //Act
+            var result = SystemUnderTest.SetMotor(new Dictionary<int, int> {{1, 1}});
+
+            //Assert
+            Assert.That(()=>result.StatusCode,Is.EqualTo(HttpStatusCode.Created));
+        }
+
+        [Test]
+        public void Test_SetRudder()
+        {
+            //Arrange 
+
+            //Act
+            var result = SystemUnderTest.SetRuder(new Dictionary<int, int> { { 1, 1 } });
+
+            //Assert
+            Assert.That(() => result.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+        }
+
+        [Test]
+        public void Test_SetIsConnected()
+        {
+            //Arrange 
+
+            //Act
+            var result = SystemUnderTest.SetIsConnected(new Dictionary<int, bool> { { 1, true } });
+
+            //Assert
+            Assert.That(() => result.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+        }
+    }
+}
