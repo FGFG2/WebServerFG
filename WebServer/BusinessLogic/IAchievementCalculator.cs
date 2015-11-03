c5ed4a3e6670c34f67d156b2fefed7d3d74c0f8c
Jonas Hashagen <jonashashagen@gmail.com>
2015-11-03 15:03:06 +0100

    Added IAchievementCalculator interface. Added BusinessLogic namespace refs #187

---
+++ WebServer/BusinessLogic/IAchievementCalculator.cs
@@ -0,0 +1,16 @@
+﻿using WebServer.Models;
+
+namespace WebServer.BusinessLogic
+{
+    /// <summary>
+    /// Interface for calculators defining one single achievement. Each achievement requires its own calculator.
+    /// </summary>
+    public interface IAchievementCalculator
+    {
+        /// <summary>
+        /// Evaluates based on the CurrentUser if he fulfills the goals and updates the database.
+        /// </summary>
+        /// <param name="targetUser">The user that needs to be evaluated.</param>
+        void CalculateAchievementProgress(SmartPlaneUser targetUser);
+    }
+}
