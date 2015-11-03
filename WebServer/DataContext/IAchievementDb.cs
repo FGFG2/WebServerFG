c93d62cd07b618e74f5219a200b2e49965256b19
Portikus <ahuisf@gmail.com>
2015-11-03 14:51:50 +0100

    Added a Interface for a database abstraction layer

---
+++ WebServer/DataContext/IAchievementDb.cs
@@ -0,0 +1,19 @@
+﻿using WebServer.Models;
+
+namespace WebServer.DataContext
+{
+    public interface IAchievementDb
+    {
+        /// <summary>
+        /// Get a SmartPlaneUser by íts userId
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        SmartPlaneUser GetSmartPlaneUserById(int userId);
+
+        /// <summary>
+        /// Save all changes to the Database
+        /// </summary>
+        void SaveChanges();
+    }
+}
