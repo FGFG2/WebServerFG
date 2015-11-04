79f7591fc5964ca71ad855249fb41bad4254cbe8
Jonas Hashagen <jonashashagen@gmail.com>
2015-11-04 23:15:12 +0100

    AchievementCalculationManager is now asynchronous ref #188

--- WebServer.Tests/BusinessLogic/AchievementCalculatorDetectorTest.cs
+++ WebServer.Tests/BusinessLogic/AchievementCalculatorDetectorTest.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using NUnit.Framework;
 using Universial.Test;
 using WebServer.BusinessLogic;
-using WebServer.Controllers;
 
 namespace WebServer.Tests.BusinessLogic
 {
--- WebServer/BusinessLogic/AchievementCalculationManager.cs
+++ WebServer/BusinessLogic/AchievementCalculationManager.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using WebServer.Models;
 
 namespace WebServer.BusinessLogic
@@ -12,24 +15,58 @@ namespace WebServer.BusinessLogic
         #region Fields
 
         private readonly IList<IAchievementCalculator> _achievementCalculators;
+        private readonly ConcurrentQueue<SmartPlaneUser> _userWithChangedData;
+        private readonly Task _achievementCalculationTask;
+        private readonly CancellationTokenSource _achievementCalculationCancelSource;
 
         #endregion;
 
         /// <summary>
-        /// Creates the Manager using the achievementDetector to find all avaialable achievements.
+        /// Creates the Manager using the achievementDetector to find all available achievements.
         /// </summary>
         /// <param name="achievementDetector">Detector used to find the available achievements</param>
         public AchievementCalculationManager(IAchievementCalculatorDetector achievementDetector)
         {
             _achievementCalculators = achievementDetector.FindAllAchievementCalculator().ToList();
+            _userWithChangedData = new ConcurrentQueue<SmartPlaneUser>();
+
+            _achievementCalculationCancelSource = new CancellationTokenSource();
+            _achievementCalculationTask = new Task(_achievementCalculation, _achievementCalculationCancelSource.Token, TaskCreationOptions.LongRunning);
+            _achievementCalculationTask.Start();
+        }
+
+        private void _achievementCalculation()
+        {
+            while (_achievementCalculationCancelSource.IsCancellationRequested == false)
+            {
+                while (_userWithChangedData.IsEmpty == false)
+                {
+                    //TODO: Add Logging if dequeTryDequeue failed
+
+                    SmartPlaneUser user;
+                    _userWithChangedData.TryDequeue(out user);
+                    _calculateAchievementsForUser(user);
+                } 
+
+                // Avoid busy-wait
+                Thread.Sleep(100);
+            }
+        }
+
+        private void _calculateAchievementsForUser(SmartPlaneUser user)
+        {
+            foreach (var achievementCalculator in _achievementCalculators)
+            {
+                achievementCalculator.CalculateAchievementProgress(user);
+            }
         }
 
         public void UpdateForUser(SmartPlaneUser userWithChangedData)
         {
-            foreach (var achievementCalculator in _achievementCalculators)
-            {
-                achievementCalculator.CalculateAchievementProgress(userWithChangedData);
-            }
+            // TODO: IAchievementCalculator, property: typeOfAchievement, otherwise it is not possible to check if achievement exists. Alternative: IsAchieved (user) method
+            _userWithChangedData.Enqueue(userWithChangedData);
         }
+
+       
     }
 }
